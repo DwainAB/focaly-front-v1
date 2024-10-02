ea100bbd7995e1a5f40bb8550b09a796ac164d95
dwain93 <74243765+dwain93@users.noreply.github.com>
2024-10-02 10:31:23 +0200

    modification style component About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import AboutStage from "../../Assets/aboutstage.png";
-import AboutDelivery from "../../Assets/aboutdelivery.png";
-import AboutSav from "../../Assets/aboutsav.png";
-import AboutPay from "../../Assets/aboutpay.png";
-import AboutProduct from "../../Assets/aboutproduct.png";
-import AboutSocial from "../../Assets/aboutsocial.png"; 
+import AboutStage from "../../Assets/aboutstage.png"
+import AboutDelivery from "../../Assets/aboutdelivery.png"
+import AboutSav from "../../Assets/aboutsav.png"
+import AboutPay from "../../Assets/aboutpay.png"
+import AboutProduct from "../../Assets/aboutproduct.png"
 
-import "./About.css";
+import "./About.css"
 
 const About = () => {
     return (
@@ -17,28 +16,28 @@ const About = () => {
                     <img src={AboutStage} alt="" />
                     <h3>Comment ça marche ?</h3>
                     <ul className='container-stage'>
-                        <li><span className='text-bold'>Etape 1 :</span> Choisir le produit, définissez vos dates de location et ajoutez vos accessoires</li><br />
-                        <li><span className='text-bold'>Etape 2 :</span> Recevoir le produit 48h avant le premier jour de location</li><br />
-                        <li><span className='text-bold'>Etape 3 :</span> Retournez les produits sous 24h avec le carton et bon de retour fournis</li><br />
+                        <li><span className='text-bold'>Etape 1 :</span>Choisir le produit, définissez vos dates de location et ajoutez vos accessoires</li><br />
+                        <li><span className='text-bold'>Etape 2 :</span>Recevoir le produit 48h avant le premier jour de location</li><br />
+                        <li><span className='text-bold'>Etape 3 :</span>Retournez les produits sous 24h avec le carton et bon de retour fournis</li><br />
                     </ul>
                 </div>
 
                 <div className="about-sujet">
                     <img src={AboutDelivery} alt="" />
                     <h3>Livraison gratuite</h3>
-                    <p>Pour faciliter votre expérience de location, la livraison est gratuite pour toute commande. Recevez votre équipement rapidement et sans frais supplémentaires. <br /><br />De plus, le retour est aussi offert grâce au bon que nous fournissons.</p>
+                    <p>Pour faciliter votre expérience de location, la livraison est gratuite pour toute commande. Recevez votre équipement rapidement et sans frais supplémentaires. <br /><br />De plus, le retour est aussi offert grâce au bon que nous fournissons</p>
                 </div>
 
                 <div className="about-sujet">
                     <img src={AboutSav} alt="" />
                     <h3>Service client réactif</h3>
-                    <p>Notre équipe dévouée est à votre disposition 7 jours sur 7 pour vous offrir un service client réactif et chaleureux. Que vous ayez besoin d'aide pour choisir le bon équipement, pour résoudre un problème technique ou pour toute autre demande, nous sommes là. <br /><br />N'hésitez pas à nous contacter : <br /> contact@focaly.com</p>
+                    <p>Notre équipe dévouée est à votre disposition 7 jours sur 7 pour vous offrir un service client réactif et chaleureux. Que vous ayez besoin d'aide pour choisir le bon équipement, pour résoudre un problème technique ou pour toute autre demande, nous sommes là <br /><br />N'hésitez pas à nous contacter : <br /> contact@focaly.com</p>
                 </div>
 
                 <div className="about-sujet">
                     <img src={AboutPay} alt="" />
                     <h3>Paiement sécurisé</h3>
-                    <p>Votre sécurité est notre priorité. Profitez de paiements 100% sécurisés pour vos locations de matériel de production. <br /><br />Vous pouvez louer notre matériel de production en toute confiance, sachant que vos informations personnelles et financières sont protégées de manière optimale.</p>
+                    <p>Votre sécurité est notre priorité. Profitez de paiements 100% sécurisés pour vos locations de matériel de production. <br /><br />Vous pouvez louer notre matériel de production en toute confiance, sachant que vos informations personnelles et financières sont protégées de manière optimale</p>
                 </div>
 
                 <div className="about-sujet">
@@ -47,11 +46,6 @@ const About = () => {
                     <p>Chez Focaly, nous vous proposons uniquement des produits professionnels, de haute qualité et équipés des derniers modèles. <br /><br />Chaque produit que nous proposons est soigneusement inspecté et régulièrement mis à jour pour répondre aux exigences les plus élevées de nos clients.</p>
                 </div>
 
-                <div className="about-sujet">
-                    <img src={AboutSocial} alt="" />
-                    <h3>Suivez-nous sur les réseaux sociaux</h3>
-                    <p>Restez connecté avec Focaly ! Suivez-nous sur nos réseaux sociaux pour découvrir nos nouveautés, des conseils, et partager vos expériences avec la communauté. <br /><br />Retrouvez-nous sur : <br /> Facebook, Instagram, Twitter.</p>
-                </div>
 
             </div>
         </div>
